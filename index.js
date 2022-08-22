f7385a33c3211303da5ae9262680a50323c75a32
mfurg <mfurgalo@gmail.com>
2022-08-22 16:05:52 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = 8080;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+  
+
+  // Join a conversation
+  const { roomId, username } = socket.handshake.query;
+  socket.join(roomId);
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, {body: `Client ${username} diconnected`});
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
